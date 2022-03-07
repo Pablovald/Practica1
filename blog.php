89b8a1a94d91d3419858236ee704735e891c6f77
Ivan zhuo <yzhuo01@ucm.es>
2022-03-07 13:45:39 +0100

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset = "UTF-8">
+  <link  rel="icon"   href="img/favicon.png" type="image/png" />
+  <title>Actividades</title>
+</head>
+<body>
+<?php require("links.php")?>
+	<h1 style="text-align:center;">BLOG</h1>
+    <div style="text-align:center;">
+        <table style="margin: 0 auto;">
+            <tr>
+                <td><h4>Texto 1</h4><br/>
+                    <img src="img/actividades1.png" alt="" width="300" height="300">
+                </td>
+                <td><h4>Texto 2</h4><br/>
+                    <img src="img/actividades1.png" alt="" width="300" height="300">
+                </td>
+                <td><h4>Texto 3</h4><br/>
+                    <img src="img/actividades1.png" alt="" width="300" height="300">
+                </td>
+            </tr>
+            <tr>
+                <td><h4>Texto 4</h4><br/>
+                    <img src="img/actividades1.png" alt="" width="300" height="300">
+                </td>
+                <td><h4>Texto 5</h4><br/>
+                    <img src="img/actividades1.png" alt="" width="300" height="300">
+                </td>
+                <td><h4>Texto 6</h4><br/>
+                    <img src="img/actividades1.png" alt="" width="300" height="300">
+                </td>
+            </tr>
+            
+        </table>
+    </div>
+
+</body>
+</html>
