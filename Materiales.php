7bdf97ba8bb27387fb83ffa0d7fa840609e38797
AlvMas01 <alvmas01@ucm.es>
2022-03-07 09:08:12 +0100

    Create Materiales.php

---
+++ Materiales.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Materiales</title>
+</head>
+
+<body>
+
+<div id="contenedor">
+
+	<?php
+		require("cabecera.php");
+	?>
+	
+	<main>
+	  <article>
+			<h1>Alquiler de Materiales</h1>
+			<p> Disponibilidad del mejor material a disposicion del cliente. </p>
+			
+			<table>
+	<tr>
+		<td>
+			<div>Imagen Material1<h4>Material1.</h4>
+			<p> Breve descripcion material1</p>
+		</td>
+		<td>
+			<div>Imagen Material2<h4>Material2.</h4>
+			<p> Breve descripcion material2</p>
+		</td>
+	</tr>
+	
+	<tr>
+		<td>
+			<div>Imagen Material3<h4>Material3.</h4>
+			<p> Breve descripcion material3</p>
+		</td>
+		<td>
+			<div>Imagen Material4<h4>Material4.</h4>
+			<p> Breve descripcion material4</p>
+		</td>
+	</tr>
+	
+	<tr>
+		<td>
+			<div>Imagen Material5<h4>Material5.</h4>
+			<p> Breve descripcion material5</p>
+		</td>
+		<td>
+			<div>Imagen Material6<h4>Material6.</h4>
+			<p> Breve descripcion material6</p>
+		</td>
+	</tr>
+
+  </table>  
+		</article>
+	</main>
+
+	<?php
+		require("pie.php");
+	?>
+	
+</div>
+</body>
+</html>
