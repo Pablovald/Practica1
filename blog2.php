ebc177f025cf6900d8702bfbe8188f101243c9e1
Ivan zhuo <yzhuo01@ucm.es>
2022-03-13 22:33:01 +0100

    a

---
+++ blog2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset = "UTF-8">
+  <link  rel="icon"   href="img/favicon.png" type="image/png" />
+  <title>Actividades</title>
+</head>
+<body>
+<?php require("links.php")?>
+    <h1>titulo del blog</h1>
+    <p> breve descripcion</p>
+    <img src="img/blogfoto.pmg" alt="" width="300" height="300">
+    <p> descripcion detallada</p>
+	
+</body>
+</html>
