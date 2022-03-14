7d0c2260ed1609b78781e2fd076b90a614f55c72
YuejieXuUCM <yuejiexu@ucm.es>
2022-03-14 12:23:02 +0100

    uluwatu

---
+++ Prac2/olaAlta.php
@@ -0,0 +1,28 @@
+<?php
+
+require ("config.php");
+
+$tituloPagina = 'Uluwatu';
+
+$tituloCabecera = 'Uluwatu';
+
+$contenidoPrincipal = <<<EOS
+<h1>Uluwatu, una de las mejores olas de la historia</h1>
+<p>Uluwatu es una combinación de los innumerables sueños y fantasías que han pasado por la mente de todo amante del surf desde la adolescencia. 
+Una izquierda idílica, prodigiosa, tan perfecta y regular que parece una ilusión óptica.</p></br>
+<div align="center"><img src = "uluwatu.png" width="500" height="400"></div></br>
+<p>Uluwatu empezó a ser surfeada en 1972, debido a la realización de Morning of the Earth, un clásico de las películas de surf.
+Después de unos días de rodaje en la playa de Kuta, encontraron Uluwatu. Para llegar a Uluwatu es necesario descender por el templo y salir por las dos rocas de la base de la cueva, 
+en la playa de Uluwatu. Steve Cooney surfeó la primera ola de la historia de Uluwatu, capturándola en una película para Morning of the Earth.</p></br>
+<h2>GUÍA PRÁCTICA ULUWATU, QUÉ VER Y QUÉ HACER OTRAS OLAS EN BALI</h2>
+<p>A Bali se la llama La isla de los dioses. Y si el surf es lo tuyo hará bien en creer en la generosidad de esos dioses. Porque Bali constituye, sin ninguna duda,
+ uno de los mejores destinos sobre la Tierra para hacer surf. Uluwatu es la rompiente más especial, la más icónica, la que más momentos de gracia pura tiene a sus espaldas. 
+ Pero, como todo el mundo sabe, hay vida, y mucha, más allá de esta izquierda que parece no cerrarse nunca. Y es que, en lo tocante a olas, Bali tiene todo cuanto puedas imaginar: 
+ reefbreaks prodigiosos, noqueantes beachbreacks, cuñas de derecha, izquierdas con espectaculares vientres tubulares…</p>
+<p>La acción se concentra en el sur de la isla. Y principalmente, en la península de Bukit, donde se encuentran las mejores olas de Bali: Uluwatu, Padang-Padang,
+ Impossibles, Bingin, Dreamland … También es una de las partes más bellas de la isla. Olas perfectas en cada rincón, aguas cristalinas, acantilados de locura, playas de arena blanca… 
+ Eso sí, como Uluwatu, muchas de esas rompientes son peligrosas. Y, sí, están masificadas, aunque en Bali la multitudes suelen ser soportables.</p>
+<p align="center"><iframe width="560" height="315" src="https://www.youtube.com/embed/UdDx4UGlJww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
+EOS;
+
+require ("plantilla.php");
