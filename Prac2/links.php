0e506eb8d93d865e87051b551eae0abd9f12e5a6
Karlitosss <cargon26@ucm.es>
2022-03-14 10:18:39 +0100

    Piscina

--- Prac2/links.php
+++ Prac2/links.php
@@ -1,10 +1,12 @@
 <div align="center">
   <p>
+   <fieldset>
 	<a href=home.php>Home</a>
     <a href=Blog.php>Blog</a>
     <a href=Actividades.php>Actividades</a>
     <a href=Alojamiento.php>Alojamiento</a>
     <a href=Materiales.php>Materiales</a>
     <a href=Perfil.php>Perfil</a>
+	 </fieldset>
   </p>
 </div>
