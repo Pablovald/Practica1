fe67479dda92d49e81c11dc83900bcdbe7831680
Karlitosss <cargon26@ucm.es>
2022-03-14 12:13:30 +0100

    Update piscinpaProfunda.php

--- Prac2/piscinpaProfunda.php
+++ Prac2/piscinpaProfunda.php
@@ -10,7 +10,7 @@ $contenidoPrincipal = <<<EOS
 <h1>Deep Dive Dubai, la piscina más grande del mundo</h1>
 <p> Unos buceadores se sumergen en Deep Dive, la piscina más profunda del mundo, en Dubai. Tiene 60 metros de profundidad y para llenarla hacen 
 falta catorce millones de litros de agua dulce.</p></br>
-<div align="center"><img src = "piscina.png" width="500" height="400"></div></br>
+<div align="center"><img src = "piscina.png" width="560" height="315"></div></br>
 <p>Además, está equipada con 56 cámaras para garantizar la seguridad. A buen seguro que la inmersión no te deja indiferente.
 La más reciente incorporación al pujante panorama deportivo y de actividades de aventura de la ciudad, Deep Dive Dubai, es el lugar ideal para 
 los amantes del agua. Abrió sus puertas en 2021 y la piscina de 60,2 metros de profundidad del centro de ocio cuenta con la certificación del Libro
@@ -22,6 +22,8 @@ desde los 10 años.</p>
 <p>La categoría Discover es para aquellos que quieran hacer una única inmersión o quieran iniciarse en una certificación de buceo. Por su parte, 
 cualquiera pueda disfrutar de la categoría Dive para probar distintas experiencias de buceo dentro de los límites de su certificación. Como su nombre
 indica, la categoría Develop proporciona formación para avanzar en sus conocimientos y nivel de certificación hasta el nivel de instructor.</p>
+<div align="center"><iframe width="560" height="315" src="https://www.youtube.com/embed/zQgNfQuYsA8" title="YouTube video player" frameborder="0" 
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 EOS;
 
 require ("plantilla.php");
