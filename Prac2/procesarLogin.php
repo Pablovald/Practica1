59a4e0abc6df119f65fa54d28d2905db7e2931cc
YuejieXuUCM <yuejiexu@ucm.es>
2022-03-16 21:33:18 +0100

    Update procesarLogin.php

--- Prac2/procesarLogin.php
+++ Prac2/procesarLogin.php
@@ -37,11 +37,8 @@ if (count($erroresFormulario) === 0) {
 		} else {
 			$fila = $rs->fetch_assoc();
 
-			if ( !password_verify($password, $fila['password'])) {
-				$erroresFormulario[] = "El usuario o el password no coinciden";
 			if ( ! password_verify($password, $fila['password'])) {
 				$erroresFormulario[] = "El password no es correcto";
-
 			} else {
 				$_SESSION['login'] = true;
 				$_SESSION['nombre'] = $fila['nombre'];
