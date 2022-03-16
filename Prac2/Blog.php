3a3477c1ad365e972c841174e6b963f4980a72e4
YuejieXuUCM <yuejiexu@ucm.es>
2022-03-16 09:36:16 +0100

    Update Blog.php

--- Prac2/Blog.php
+++ Prac2/Blog.php
@@ -14,50 +14,47 @@ $contenidoPrincipal = <<<EOS
 	<tr>
 		<td>
 			<div align = "center">
-				<img src= 'piscina.png' width='250' height='250'>
+				<a href="piscinpaProfunda.php"><img src= 'piscina.png' width='250' height='250'></a>
 				<h4>La piscina más profunda del mundo está en Dubai.</h4>
 				<p>Unos buceadores se sumergen en Deep<br>
 				Dive, la piscina más profunda del mundo,<br>
-				en Dubai.</p>
-				<a href="piscinpaProfunda.php"> Leer más</a>
+				en Dubai... <a href="piscinpaProfunda.php"> Leer más</a></p>
+				
 			</div>
 		</td>
 		<td>
 			<div align = "center">
-				<img src= 'ola.jpg' width='250' height='250'>
+				<a href="olaAlta.php"><img src= 'ola.jpg' width='250' height='250'></a>
 				<h4>Uluwatu, una de las mejores olas de la historia.</h4>
 				<p>Uluwatu es una combinación de los <br>
 				innumerables sueños y fantasías que han <br>
-				pasado por la mente de todo amante del <br>
-				surf desde la adolescencia.</p>
-				<a href="olaAlta.php">Leer más</a>
+				pasado por la mente... <a href="olaAlta.php">Leer más</a></p>
+				
 			</div>
 		</td>
 		<td>
 			<div align = "center">
-				<img src= 'barreraCoral.jpg' width='250' height='250'>
+				<a href="barreraCoral.php"><img src= 'barreraCoral.jpg' width='250' height='250'></a>
 				<h4>La increíble visita a la Gran Barrera de Coral.</h4>
 				<p>El mayor arrecife de coral del mundo <br>
 				se encuentra frente a la costa de <br>
-				Queensland, en el llamado mar del Coral</p>
-				<a href="barreraCoral.php">Leer más</a>
+				Queensland... <a href="barreraCoral.php">Leer más</a></p>
 			</div>
 		</td>
 	</tr>
 	<tr>
 		<td>
 			<div align = "center">
-				<img src= 'regata.jpg' width='250' height='250'>
+				<a href="regata.php"><img src= 'regata.jpg' width='250' height='250'></a>
 				<h4>Regata, tres meses a solas con el mar.</h4>
 				<p>La Vendée Globe es una regata que <br>
 				consiste en dar la vuelta al mundo a vela<br>
-				en solitario, sin escalas y sin asistencia.</p>
-				<a href="regata.php"> Leer más</a>
+				en solitario... <a href="regata.php"> Leer más</a></p>
 			</div>
 		</td>
 		<td>
 			<div align = "center">
-				<img src= 'record.jpg' width='250' height='250'>
+				<a href="record.php"><img src= 'record.jpg' width='250' height='250'></a>
 				<h4>Récord mundial nadando, 127 kilómetros en solitario.</h4>
 				<p></p>
 				<a href="record.php">Leer más</a>
@@ -65,12 +62,12 @@ $contenidoPrincipal = <<<EOS
 		</td>
 		<td>
 			<div align = "center">
-				<img src= 'kayakExtremo.png' width='250' height='250'>
+				<a href="kayakExtremo.php"><img src= 'kayakExtremo.png' width='250' height='250'></a>
 				<h4>Jötunn, el kayak más extremo</h4>
 				<p>Jötunn es un documental en el que se<br>
 				ha intentado enseñar una aventura de <br>
-				exploración desde dentro.</p>
-				<a href="kayakExtremo.php">Leer más</a>
+				exploración... <a href="kayakExtremo.php">Leer más</a></p>
+				
 			</div>
 		</td>
 	</tr>
