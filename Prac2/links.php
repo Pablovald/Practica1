a91c0887ea324ebfeced9e0e6537cbefffe857c1
Karlitosss <cargon26@ucm.es>
2022-03-16 10:17:40 +0100

    Update links.php

--- Prac2/links.php
+++ Prac2/links.php
@@ -4,9 +4,7 @@
 	<a href=home.php>Home</a>
     <a href=Blog.php>Blog</a>
     <a href=Actividades.php>Actividades</a>
-    <a href=Alojamiento.php>Alojamiento</a>
-    <a href=Materiales.php>Materiales</a>
-    <a href=Perfil.php>Perfil</a>
+	<a href=Contacto.php>Contacto</a>
 	 </fieldset>
   </p>
 </div>
