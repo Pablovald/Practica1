4073401079d744fef720ab93b0989d146e5981c0
YuejieXuUCM <yuejiexu@ucm.es>
2022-03-17 23:19:51 +0100

    Update procesarInscripcion.php

--- Prac2/procesarInscripcion.php
+++ Prac2/procesarInscripcion.php
@@ -12,29 +12,54 @@ $tituloCabecera = 'Inscripcion';
 
 $contenidoPrincipal = '';
 
-$capacidad = isset($_POST['capacidad']);
+$nombreActividad = isset($_POST['actividad']) ? $_POST['actividad'] : null;
 $solicitud_dia = isset($_POST['dia']) ? $_POST['dia'] : null;
-echo "$solicitud_dia";
+$nombreUsuario = isset($_SESSION['nombre']) ? $_SESSION['nombre'] : null;
+$cursoActividad = isset($_POST['curso']) ? $_POST['curso'] : null;
+
 if (isset($_SESSION["login"])) {
-	if(!empty($solicitud_dia)){
-		if($capacidad > 0){
-			$contenidoPrincipal .= <<<EOS
-			<h1>¡Felicidades {$_SESSION['nombre']} ! Te has inscrito correctamente</h1>
-			EOS;
+	$conn = $app->conexionBd();
+	
+	$id=sprintf("SELECT id FROM Usuarios U WHERE U.nombreUsuario = '%s'", $conn->real_escape_string($nombreUsuario));
+	$rs = $conn->query($id);
+	$tamListaAct=sprintf("SELECT * FROM ListaActividades");
+	$rs1 = $conn->query($tamListaAct);
+	if($rs && $tamListaAct){
+		$capacidad = sprintf("SELECT * FROM ListaActividades LA WHERE LA.dia = '%s' AND LA.nombre = '%s' AND LA.curso = '%s'"
+					, $conn->real_escape_string($solicitud_dia)
+					, $conn->real_escape_string($nombreActividad)
+					, $conn->real_escape_string($cursoActividad));
+		$rs2 = $conn->query($capacidad);
+		if($rs2){
+			$row = $rs->fetch_assoc();
+			if($rs2->num_rows < 5){
+				$query=sprintf("INSERT INTO ListaActividades(nombre, ID, dia, idUsuario, curso) VALUES('%s', '%s', '%s', '%s', '%s')"
+						, $conn->real_escape_string($nombreActividad)
+						, $conn->real_escape_string($rs1->num_rows)
+						, $conn->real_escape_string($solicitud_dia)
+						, $conn->real_escape_string($row['id'])
+						, $conn->real_escape_string($cursoActividad));
+				$rs3 = $conn->query($query);
+				if($rs3){
+					echo "Inscrito correctamente";
+				} else{
+					echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+					exit();
+				}
+			} else {
+				echo "$nombreActividad del $cursoActividad en el $solicitud_dia están agotados, por favor seleccione otra fecha";
+			}
+
+		} else {
+			echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+			exit();
 		}
-		else{
-			$contenidoPrincipal .= <<<EOS
-			<h1>Lo sentimos no quedan plazas disponibles para el dia seleccionado</h1>
-			EOS;
-		}
+	} else {
+		echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+		exit();
 	}
-	else{
-	$contenidoPrincipal .= <<<EOS
-	<h1>¡Lo sentimos no disponemos de actividad para el dia deseado.</h1>
-	EOS;
-	}
-} else {
-	//<form action="procesarInscripcion.php" method="POST"> (va debajo de EOS)
+} 
+else {
 	$contenidoPrincipal .= <<<EOS
 	<h1>Necesitas estar registrado en nuestra página web para inscribirte en alguna actividad. Si ya tienes una cuenta, inicia sesión.</h1>
 	EOS;
