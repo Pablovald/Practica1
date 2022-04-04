3d5a43b5ee353d1f294ceabd507bb6440744ca50
Karlitosss <cargon26@ucm.es>
2022-04-04 11:58:10 +0200

    Update registro.php

--- Prac2/registro.php
+++ Prac2/registro.php
@@ -7,23 +7,32 @@ $tituloPagina = 'Registro';
 $tituloCabecera = 'Registro';
 
 $contenidoPrincipal = <<<EOS
-<h1>Registro de usuario</h1>
-
-<form action="procesarRegistro.php" method="POST">
-<fieldset>
-	<div class="grupo-control">
-		<label>Nombre de usuario:</label> <input class="control" type="text" name="nombreUsuario" />
+<div class="login">
+	<div class="login-screen">
+		<div class="app-title">
+			<h1>Registro</h1>
+		</div>
+		
+		<div class="login-form">
+			<form action="procesarRegistro.php" method="POST">
+				<div class="grupo-control">
+				<input class="control" type="text" name="nombreUsuario" placeholder="Nombre usuario" />
+				</div>
+				<div class="grupo-control">
+				<input class="control" type="text" name="nombre" placeholder="Nombre completo" />
+				</div>
+				<div class="grupo-control">
+				<input class="control" type="password" name="password" placeholder="Contraseña" />
+				</div>
+				<div class="grupo-control"><input class="control" type="password" name="password2" placeholder="Verifica tu contraseña" /><br /></div>
+				<div class="grupo-control">
+				<button type="submit" name="registro">Registrar</button>
+				</div>
+			</form>
+		</div>
 	</div>
-	<div class="grupo-control">
-		<label>Nombre completo:</label> <input class="control" type="text" name="nombre" />
-	</div>
-	<div class="grupo-control">
-		<label>Password:</label> <input class="control" type="password" name="password" />
-	</div>
-	<div class="grupo-control"><label>Vuelve a introducir el Password:</label> <input class="control" type="password" name="password2" /><br /></div>
-	<div class="grupo-control"><button type="submit" name="registro">Registrar</button></div>
-</fieldset>
+</div>
 </form>
 EOS;
 
-include __DIR__.'/includes/plantillas/plantilla.php';
+include __DIR__.'/includes/plantillas/plantillaLogin.php';
