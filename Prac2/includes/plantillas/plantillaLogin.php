d24adff807fef961b7ba0d1fbbd4e13df24902fb
Karlitosss <cargon26@ucm.es>
2022-04-06 11:12:01 +0200

    css login

--- Prac2/includes/plantillas/plantillaLogin.php
+++ Prac2/includes/plantillas/plantillaLogin.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link  rel="icon" href="img/favicon.png" type="image/png" />
-	<link rel="stylesheet" type="text/css" href="Login.css" />
+	<link rel="stylesheet" type="text/css" href="estiloLogin.css" />
     <title><?= $tituloPagina ?></title>
 </head>
 <body>
