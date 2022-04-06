d47606b75aace8c7766ec3e4bd885770f3ae0985
Pablovald <pablovaldesbalduz@gmail.com>
2022-04-06 11:03:16 +0200

    botones para actividades y alojamientos

--- Prac2/material.php
+++ Prac2/material.php
@@ -7,17 +7,22 @@ $conn = $app->conexionBd();
 $tablaMaterial=sprintf("SELECT * FROM Materiales M WHERE M.nombre LIKE '$tituloPagina' ");
 $row = $conn->query($tablaMaterial);
 $rs=$row->fetch_assoc();
-$Cont="<h3> $tituloPagina </h3>
-<img src= $rs[imagen] width='350' height='350'>
-<p> Descripción detallada del producto: </p>
-<p>"."$rs[desc_det]</p>
-<p>"." Precio del producto: "." $rs[precio]</p>
-<link rel='stylesheet' href='style.css'>
+$Cont="
+<div id='imagen'>
+    <h3> $tituloPagina </h3>
+    <img src= $rs[imagen] width='350' height='350'>
+</div>
+<div id='contenido'>
+<p> Descripción detallada del producto: </p><br/>
+<p>"."$rs[desc_det]</p><br/>
+<p>"." Precio del producto: "." $rs[precio] "." €</p>
+<link rel='stylesheet' href='material.css'>
 <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
 <button class='carrito'>
     <span>Carrito</span>
     <i class='fa fa-shopping-basket' aria-hidden='true'></i>
-</button>";
+</button>
+</div>";
 
 $contenidoPrincipal = <<<EOS
     $Cont
