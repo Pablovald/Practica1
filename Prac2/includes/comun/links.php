20f13b5d8f2d5c35b44359f41ce7b304c76fcd5a
YuejieXuUCM <yuejiexu@ucm.es>
2022-04-10 19:27:59 +0200

    Update links.php

--- Prac2/includes/comun/links.php
+++ Prac2/includes/comun/links.php
@@ -3,5 +3,5 @@
     <a href=Blog.php>Blog</a>
     <a href=Actividades_Main.php>Actividades</a>
 	<a href=Materiales.php>Materiales</a>
-	<a href=Alojamientos.php>Alojamientos</a>
+	<a href=Alojamientos_Main.php>Alojamientos</a>
 </nav>
