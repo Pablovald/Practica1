686a8694b0a8356aa1984e97b99c4599da5dcbff
YuejieXuUCM <yuejiexu@ucm.es>
2022-04-10 20:00:53 +0200

    Actividad

--- Prac2/includes/Actividad.php
+++ Prac2/includes/Actividad.php
@@ -29,6 +29,51 @@ class Actividad
         }
     }
 
+    public static function infoActividad(&$tituloPagina, &$tituloCabecera){
+        $tituloPagina = htmlspecialchars($_GET["actividad"]);
+        $tituloCabecera = strtoupper($tituloPagina);
+        $contenidoPrincipal ="";
+        
+        $app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+        $tablaActividad=sprintf("SELECT * FROM Actividades A WHERE A.nombre LIKE '%s' "
+						, $conn->real_escape_string($tituloPagina));
+        $row = $conn->query($tablaActividad);
+        if($row){
+	        $rs=$row->fetch_assoc();
+	        $Cont="<h3>Información del curso de "."$tituloPagina".":</h3>
+	        <p>"."$rs[info]"."</p>
+	        <h4> Horarios disponibles </h4>
+	        <p> Lunes a Viernes de 16:00 a 18:00 </p>
+	        <p> Sabado y Domingo de 11:30 a 13:30</p>
+	        <p> Los cursos, por lo normal, se realizarán impartiendo una única clase semanal (ampliable a 2 semanales en el caso de los cursos completos). </p>
+	        <h4> Precios del curso </h4>";
+	
+	        $row=$conn->query(sprintf("SELECT C.nombre_curso,C.precio FROM CursosActividades C WHERE C.nombre_actividad LIKE '%s'"
+								, $conn->real_escape_string($tituloPagina)));
+	        if($row)
+	        {
+	        	for($i=0;$i<$row->num_rows;$i++){
+		    	    $act=$row->fetch_assoc();
+		    	    $Cont.="<p>"."$act[nombre_curso]".": "."$act[precio]"." €</p>";
+		        }
+                $contenidoPrincipal = <<<EOS
+                    $Cont
+                EOS;
+		        $row->free();
+	        }
+	        else{
+		        echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+		        exit();
+	        }
+        }
+        else{
+	        echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+	        exit();
+        }
+        return $contenidoPrincipal;
+    }
+
     public static function inscribirActividad($nombreActividad, $solicitud_dia, $cursoActividad, &$result){
         $nombreUsuario = isset($_SESSION['nombreUsuario']) ? $_SESSION['nombreUsuario'] : null;
 
