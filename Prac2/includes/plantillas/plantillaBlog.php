7be152fd116e9c43d2b1bfa96bce367c33fe8f51
Ivan zhuo <yzhuo01@ucm.es>
2022-04-12 18:37:48 +0200

    a

--- Prac2/includes/plantillas/plantillaBlog.php
+++ Prac2/includes/plantillas/plantillaBlog.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link  rel="icon" href="img/favicon.png" type="image/png" />
 	<link rel="stylesheet" type="text/css" href="home.css" />
     <title><?= $tituloPagina ?></title>
 
