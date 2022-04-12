d46d0b57b4b2599863157deed68e2ae99e58a45a
AlvMas01 <alvmas01@ucm.es>
2022-04-12 12:57:55 +0200

    errores css

--- Prac2/includes/comun/links.php
+++ Prac2/includes/comun/links.php
@@ -1,8 +1,10 @@
 <nav>
-	<a href=home.php>Home</a>
-    <a href=Blog.php>Blog</a>
-    <a href=Actividades_Main.php>Actividades</a>
-	<a href=Materiales.php>Materiales</a>
-	<a href=Alojamientos_Main.php>Alojamientos</a>
-	<a href=Carrito.php>Carrito</a>
+<ul>
+	<li><a href=home.php>Home</a></li>
+    <li><a href=Blog.php>Blog</a></li>
+    <li><a href=Actividades_Main.php>Actividades</a></li>
+	<li><a href=Materiales.php>Materiales</a></li>
+	<li><a href=Alojamientos_Main.php>Alojamientos</a></li>
+	<li><a href=Carrito.php>Carrito</a></li>
+</ul>
 </nav>
