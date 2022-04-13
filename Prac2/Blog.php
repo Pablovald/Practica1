8bdacef303ddafe60d12f1736a60b07813380e86
Karlitosss <cargon26@ucm.es>
2022-04-13 18:50:45 +0200

    entradasblogg css

--- Prac2/Blog.php
+++ Prac2/Blog.php
@@ -1,5 +1,5 @@
 <head>
-<link rel="stylesheet" type="text/css" href="BlogStyle.css" />
+<link rel="stylesheet" type="text/css" href="BlogView.css" />
 </head>
 
 <?php
--- Prac2/includes/comun/navBlog.php
+++ Prac2/includes/comun/navBlog.php
@@ -1,6 +1,4 @@
-<head>
-<link rel="stylesheet" type="text/css" href="Vistablog.css" />
-</head>
+
 
 <?php
 
