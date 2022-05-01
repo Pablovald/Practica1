ed467d106c3f3e0ef9a6ad4aaaf956acceb4f279
Karlitosss <cargon26@ucm.es>
2022-05-01 12:26:17 +0200

    Blog

--- Prac2/includes/plantillas/plantillaBlog.php
+++ Prac2/includes/plantillas/plantillaBlog.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link  rel="icon" href="img/favicon.png" type="image/png" />
-	<link rel="stylesheet" type="text/css" href="HomeView.css" />
+	<link rel="stylesheet" type="text/css" href="ViewerHome.css" />
 	<link rel="stylesheet" type="text/css" href="BlogView.css" />
 	
     <title><?= $tituloPagina ?></title>
