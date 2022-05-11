74d85e51ebe88504332bb41fec0df56e633289e3
YuejieXuUCM <yuejiexu@ucm.es>
2022-05-11 12:42:09 +0200

    Vista Material

--- Prac2/Materiales.php
+++ Prac2/Materiales.php
@@ -1,7 +1,9 @@
 <?php
-require_once __DIR__. '/includes/config.php';
+namespace es\fdi\ucm\aw;
+include __DIR__.'/includes/GeneraVistas.php';
+require_once __DIR__.'/includes/config.php';
 
-$contenidoPrincipal = es\fdi\ucm\aw\Material::materialMain($tituloPagina, $tituloCabecera);
+$contenidoPrincipal = materialMain($tituloPagina, $tituloCabecera);
 
 
 include __DIR__. '/includes/plantillas/plantilla.php';
